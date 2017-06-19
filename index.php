9880d20369530a827a46cd5d80b15378d58cffdb
vans <vansindata@gmail.com>
2017-06-19 16:00:19 +0800

    modify Front-end layout

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 require_once 'inc.php';
 $ARRAY_VALID_DIR = array('shift', 'scroll', 'mouse');
+#print_r($_POST); print_r($_FILES);
 
-if(!empty($_POST['submit']) && !empty($_POST['dir']) && !empty($_FILES['image']) && $_FILES['image']['error'] == 0) {
+if(!empty($_POST['dir']) && !empty($_FILES['image']) && $_FILES['image']['error'] == 0) {
     // https://stackoverflow.com/questions/38509334/full-secure-image-upload-script
     $uploaddir = 'uploads/';
     #print_r($_POST);print_r($_FILES);
@@ -55,25 +56,75 @@ if(!empty($_POST['submit']) && !empty($_POST['dir']) && !empty($_FILES['image'])
   <meta charset="UTF-8">
   <title>Upload Page</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href=vendors/bootstrap-3.3.7/css/bootstrap.min.css />
+<script src=vendors/jquery-3.2.1.min.js></script>
+<script src=vendors/bootstrap-3.3.7/js/bootstrap.min.js></script>
+<script src=vendors/bootstrap-3.3.7/js/bootstrap-filestyle.min.js></script>
+<style>
+.glyphicon.spinning{
+animation: spin 1s infinite linear;
+-webkit-animation: spin2 1s infinite linear;
+}
+@keyframes spin {
+    from { transform: scale(1) rotate(0deg); }
+    to { transform: scale(1) rotate(360deg); }
+}
+@-webkit-keyframes spin2 {
+    from { -webkit-transform: rotate(0deg); }
+    to { -webkit-transform: rotate(360deg); }
+}
+</style>
 </head>
 <body>
+<div class=container>
+<div class=row>
+<div class="col-lg-8 col-lg-offset-2">
+<h1 class="page-header">Gazing Demo Page</h1>
+<ul>
+<li>Supported image extensions: <code>.png</code>, <code>.jpg</code>, <code>.jpeg</code></li>
+<?php /*
+<li>Maximum file size: <tt>300kB</tt>.</li>
+<li>Maximum image dimension: <tt>400px</tt>. Will be resized if exeeds the limit.</li>
+<li>Minimum image dimensions <i>(after resize)</i>: <tt>200x25px</tt>.</li>
+*/?>
+</ul>
+<form id="submit-form" class="form-inline" method="post" enctype="multipart/form-data">
+<div class="form-group">
+<div class="input-group">
+<input type="file" name="image" class="filestyle" data-icon="false" data-buttonBefore="true">
+</div>
+</div> &nbsp;
+<?php foreach($ARRAY_VALID_DIR as $k=>$v){ ?>
+<button class="btn btn-default btn-dir" type="submit" name="dir" value="<?php echo$v?>">Let's <?php echo$v?>!</button>
+<?php } ?>
+</form>
+
+<div style=height:16px></div>
+
+<div id="status-progress" class="alert alert-info" role="alert" style="display: none;"><span class="glyphicon glyphicon-refresh spinning"></span> <b>Wait for it...</b>
+</div>
+
+<script>
+$('#submit-form').submit(function(){
+  $('#status-progress').css('display','');
+});
+</script>
+
+<?php if(isset($py_output) and $py_output){ ?>
+<div id="output" class="panel panel-success" style="">
+<div class="panel-heading"><b>Result</b></div>
+<div class="panel-body" style="overflow-x: scroll;">
 <?php
-if(isset($py_output) and $py_output){
-echo'<h1 style="border-bottom:1px solid #aaa">Result</h1>';
 $GAZE_OUTPUT_PATH = GAZE_PYTHON_OUTPUT_FOLDER . '/';
 list($tmp, $path) = explode($GAZE_OUTPUT_PATH, $py_output);
 $src = WEB_URL . $GAZE_OUTPUT_PATH . $path;
 ?>
 <video width="320" autoplay loop controls><source src="<?php echo $src?>" type="video/mp4">Your browser does not support the video tag with MP4.</video>
 <div style=height:30px></div>
+</div><?php #.panel-body?>
+</div><?php #.panel?>
 <?php } ?>
-<h1 style="border-bottom:1px solid #aaa">Upload</h1>
-<form method="post" enctype="multipart/form-data">
-<h2>Direction:</h2>
-<?php foreach($ARRAY_VALID_DIR as$k=>$v){ ?>
-<label><input type=radio name=dir value=<?php echo $v?><?php echo $k==0?' checked':''?> /><?php echo$v?></label>
-<?php } ?>
-<h2>Image:</h2>
-<input name="image" type="file" />
-<input style="display:block;max-width:280px;width:100%;font-size:18px;height:60px;margin:30px 0 0" name="submit" type="submit" value="Upload!" />
-</form>
+
+</div><?php #.col-lg?>
+</div><?php #.row?>
+</div><?php #.container?>
