ec03cf12b323856cf1d8cab0ad91eceeefeeba57
datascitw <datasci.tw.team@gmail.com>
2017-07-07 15:51:04 +0800

    Integrated and tested on windows

--- index.php
+++ index.php
@@ -51,10 +51,11 @@ if(!empty($_POST['dir']) && !empty($_FILES['image']) && $_FILES['image']['error'
     if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile)) {
 
        $abs_uploadfile = __DIR__  . '/' . $uploadfile;
+       #echo 'absolute uploaded image path: '.($abs_uploadfile);#debug
        $abs_mp4file = get_mp4_file_abs_path($abs_uploadfile);
 
        $py_output = '';
-       $MAX_N = 10;
+       $MAX_N = 10;$n=0;
        while(true){
           if($py_output or $n>=$MAX_N){break;}
           if(file_exists($abs_mp4file)){
@@ -150,7 +151,7 @@ $('#submit-form').submit(function(){
 <?php
 #$GAZE_OUTPUT_PATH = GAZE_PYTHON_OUTPUT_FOLDER . '/';
 list($tmp, $path) = explode(UPLOAD_IMAGE_PATH, $py_output);
-$src = WEB_URL . UPLOAD_IMAGE_PATH. $path;
+$src = UPLOAD_IMAGE_PATH. $path;
 ?>
 <video width="320" autoplay loop controls><source src="<?php echo $src?>" type="video/mp4">Your browser does not support the video tag with MP4.</video>
 <div style=height:30px></div>
